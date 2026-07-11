52c8472f87cc8af75b7cb7953a70de4482e3cb04
LePeruvienn <koto91@free.fr>
2026-07-11 11:59:47 +0200

    Make small changes

---
+++ FEURcraft/utils/include/ptr_helper.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_PTR_HELPER_H
+#define UTILS_PTR_HELPER_H
+
+#define CHECK_IS_NULL(ptr, error_msg) \
+	if (ptr == NULL) \
+	{ \
+		LOG_ERROR(error_msg); \
+	}
+
+#define FREE_PTR_NOT_NULL(ptr, free_func) \
+	if (ptr != NULL) \
+	{ \
+		free_func(ptr); \
+		ptr = NULL; \
+	}
+
+#endif // UTILS_PTR_HELPER_H
