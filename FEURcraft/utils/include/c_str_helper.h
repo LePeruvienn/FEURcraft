e74e81f44c56f5eee2507aeed19cc38761ec99fb
LePeruvienn <koto91@free.fr>
2026-07-11 10:34:38 +0200

    Mise à jour des utils

---
+++ FEURcraft/utils/include/c_str_helper.h
@@ -0,0 +1,9 @@
+#ifndef C_STR_HELPER_H
+#define C_STR_HELPER_H
+
+void c_str_to_upper(char* str);
+void c_str_to_lower(char* str);
+
+void c_str_is_empty(const char* str);
+
+#endif // C_STR_HELPER_H
